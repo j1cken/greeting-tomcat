f3cba960e1ec4ac2e7e1da2bd2fc8e1495800e27
Torben Jaeger <jicken@gmail.com>
2021-06-02 15:46:35 +0200

    Update GreetingEndpoint.java

--- src/main/java/com/example/demo/GreetingEndpoint.java
+++ src/main/java/com/example/demo/GreetingEndpoint.java
@@ -25,7 +25,7 @@ import javax.ws.rs.QueryParam;
 @Path("/greet")
 public class GreetingEndpoint {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hola, %s!";
 
     @GET
     @Produces("application/json")
